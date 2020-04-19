c3c3e1d477a7a57c4656505adf92e2f4a17d21cd
konomi_casa <konomi360@gmail.com>
2020-04-19 21:16:57 +0200

    Code comments and documentation

--- MDT/io_management.py
+++ MDT/io_management.py
@@ -9,8 +9,15 @@ TAG_SOCAL="socal"
 TAG_TEXT= "comment"
 
 
-#TODO Rework header reading
+#TODO Rework header reading to return it somehow
+
 def read_og_set_file(file_name):
+    '''
+    This function reads a .csv file with the field descriptions in the first line and returns an array
+    of dictionaries with the entries in the file
+    :param file_name: The path to the csv file
+    :return: array of dictionaries with all the entries in the file
+    '''
     entries=[]
     f = open(file_name, "r")
 
@@ -36,11 +43,18 @@ def read_og_set_file(file_name):
 
 
 def split_set(entries):
+    '''
+    This function splits a set of examples in accordance to their performance with diferent training methods
+    (labeled TAG_SVR and TAG_SOCAL)
+    :param entries: All mixed class entries
+    :return: A list with two sublists, the first corresponding to the SVR instances, the second corresponding to the
+    SOCAL instances.
+    '''
     set1=[]
     set2=[]
     sets=[set1, set2]
     for entry in entries:
-        if entry[TAG_SVR]>entry[TAGTAG_SOCAL]:
+        if entry[TAG_SVR]>entry[TAG_SOCAL]:
             set1.append(entry)
         else:
             set2.append(entry)
@@ -49,7 +63,12 @@ def split_set(entries):
 
 
 def get_entry(id, set):
-
+    '''
+    This function searches for an entry in a given set that matches a given id
+    :param id: The id to search (int)
+    :param set: A set of entries
+    :return: The entry in case the id exists, None if not found
+    '''
     i=0
     entry=set[i]
     max_len=len(set)
@@ -63,7 +82,7 @@ def get_entry(id, set):
         return entry
 
 #This method joins two sets that are linked through the field id. We search for the same ID in both sets and join said entries to create a complete set
-def join_partial_set_entries(set_comments, set_results):
+#def join_partial_set_entries(set_comments, set_results):
     
 
 
