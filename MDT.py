824873792675858a7dc4d5af12c029b9a1f0cc59
konomi_casa <konomi360@gmail.com>
2020-05-11 23:57:34 +0200

    - Refactoring of main class into functions - Testing SQLite methods - Planned SQL storage of attributes to move to ANNA - Planned table structure for user reviews combinations

--- MDT.py
+++ MDT.py
@@ -1,5 +1,6 @@
 import nltk
 import timeit
+import sqlite3
 
 from attribute_management import generate_attributes
 from constants import REVSET
@@ -12,72 +13,93 @@ from att_generators.void_attr_gen import VoidAttGen
 
 RUTA_BASE = 'ficheros_entrada/'
 
-# TODO: una integración de textos de un UserCase
+
+
 # TODO: Crear un fichero de set nuevo de user cases, una sola linea de texto, usuario, maeps o clase directamente, y review texto (el texto a parte quiza)
-# TODO: usar el nuevo set para attribute_management y seguir adelante como planeado
+# TODO: Base de datos sql para guardar las combinaciones de usertexts, leer de ahi.
+
+def setup_nltk():
+    try:
+        nltk.data.find('tokenizers/punkt')
+    except LookupError:
+        nltk.download('punkt')
+
+    try:
+        nltk.data.find('taggers/averaged_perceptron_tagger')
+    except LookupError:
+        nltk.download('averaged_perceptron_tagger')
+
+    try:
+        nltk.data.find('taggers/universal_tagset')
+    except LookupError:
+        nltk.download('universal_tagset')
 
 
-if __name__ == "__main__":
+def generate_genlist():
+    list_generators = [VoidAttGen.gen_verb(), VoidAttGen.gen_det(), VoidAttGen.gen_pron(), VoidAttGen.gen_sentences(),
+                       VoidAttGen.gen_wc(), VoidAttGen.gen_noun(), VoidAttGen.gen_adj()]
+    return list_generators
+
+
+def load_usercase_set():
     entries_socal_app = read_partial_set(RUTA_BASE + 'result-IMDB-SOCAL.txt')
     entries_svr_app = read_partial_set(RUTA_BASE + 'result-IMDB-SVR62.txt')
     entries_comments = read_partial_set(RUTA_BASE + 'revs_imdb.txt', REVSET)
     complete_results = assing_class(join_result_sets(entries_socal_app, entries_svr_app))
-    user_cases = join_partial_set_entries(complete_results, entries_comments)
+    return join_partial_set_entries(complete_results, entries_comments)
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect('example.db')
+
+    c = conn.cursor()
+    # c.execute("DROP TABLE texto_combi")
+    # c.execute("CREATE TABLE texto_combi (tid INTEGER PRIMARY KEY AUTOINCREMENT, uid text, revnum int, texto text)")
+
+    c.execute("Insert into texto_combi (uid, revnum, texto) values ('paco', 9, 'esta es la review de paco, finjamos que son 9')")
+    c.execute("select * from texto_combi")
+    for row in c.execute("select * from texto_combi"):
+        print(row)
+    c.close()
+    conn.commit()
+
+
+
+
+
+
 
 
     # print(len(user_cases))
 
-    @chronometer
-    def test():
-        user_case = list(user_cases.items())[0][1]
-        texto = user_case.get_text()
-        # print("Longitud del texto: %d" % len(texto))
-        list_generators = []
-        gen_tmp = VoidAttGen.gen_wc()
-        gen_noun = VoidAttGen.gen_noun()
-        gen_adj = VoidAttGen.gen_adj()
-        list_generators.append(VoidAttGen.gen_verb())
-        list_generators.append(VoidAttGen.gen_det())
-        list_generators.append(VoidAttGen.gen_pron())
-        list_generators.append(VoidAttGen.gen_sentences())
-
-        list_generators.append(gen_tmp)
-        list_generators.append(gen_noun)
-        list_generators.append(gen_adj)
-
-        dict_temporal = {user_case.get_id(): user_case}
-        # generate_attributes(dict_temporal, entries_comments, list_generators)
-
-        print(user_case.attributes)
-
-
-    test()
-    print_chrono()
-
-    # for tupla in user_cases.items():
-    #     user = tupla[1]
+    # @chronometer
+    # def test():
+    #     user_case = list(user_cases.items())[0][1]
+    #     texto = user_case.get_text()
+    #     # print("Longitud del texto: %d" % len(texto))
+    #     list_generators = []
+    #     gen_tmp = VoidAttGen.gen_wc()
+    #     gen_noun = VoidAttGen.gen_noun()
+    #     gen_adj = VoidAttGen.gen_adj()
     #
-    #     print("Maeps %s %s" % (user.get_maep_socal(), user.get_maep_svr()))
+    #     list_generators.append(gen_tmp)
+    #     list_generators.append(gen_noun)
+    #     list_generators.append(gen_adj)
+    #
+    #     dict_temporal = {user_case.get_id(): user_case}
+    #     # generate_attributes(dict_temporal, entries_comments, list_generators)
+    #
+    #     print(user_case.attributes)
+
+
+    # test()
+    # print_chrono()
 
 # print(len(sorted_by_class[TAG_SOCAL]))
 
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
 
-try:
-    nltk.data.find('taggers/averaged_perceptron_tagger')
-except LookupError:
-    nltk.download('averaged_perceptron_tagger')
-
-try:
-    nltk.data.find('taggers/universal_tagset')
-except LookupError:
-    nltk.download('universal_tagset')
-
-tokens = nltk.word_tokenize("My name is Phillip Douglass, the most beautilful in this amazing world, bitch.")
-
-tagged = nltk.pos_tag(tokens, tagset='universal')
-
-print(tagged)
+# tokens = nltk.word_tokenize("My name is Phillip Douglass, the most beautilful in this amazing world, bitch.")
+#
+# tagged = nltk.pos_tag(tokens, tagset='universal')
+#
+# print(tagged)
